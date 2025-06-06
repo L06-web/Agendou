da6508a6d8feef58d8f710c2046873a6b3f61b94
L06-web <lucasdanielpinheiro825@gmail.com>
2025-06-05 21:32:53 -0300

    Update login.js

--- login.js
+++ login.js
@@ -3,7 +3,7 @@ document.getElementById('login-form').addEventListener('submit', function(e) {
   const email = document.getElementById('email').value;
   const senha = document.getElementById('password').value;
 
-  if (email === "lucasdanielpinheiro825@gmail.com" && senha === "123456") {
+  if (email === "admin@teste.com.br" && senha === "123456") {
     window.location.href = 'index1.html';
   } else {
     Swal.fire("Erro", "Credenciais inválidas", "error");
